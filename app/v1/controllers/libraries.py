83e9c82fd338acc4b566a3ee174815dbacbf8137
kujyp <pjo901018@gmail.com>
2018-04-09 19:30:35 +0900

    Separate protected fields (#10)

--- app/v1/controllers/libraries.py
+++ app/v1/controllers/libraries.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import (Resource, reqparse, fields,
-                           marshal_with, Api)
+                           marshal_with, Api, marshal)
 from sqlalchemy.exc import IntegrityError
 
 from app.database import db
@@ -15,9 +15,6 @@ library_fields = {
     'location_number': fields.String,
     'location_detail': fields.String,
 
-    'manager_name': fields.String,
-    'manager_email': fields.String,
-    'manager_phone': fields.String,
     'audiences': fields.String,
 
     'fac_beam_screen': fields.Boolean,
@@ -29,6 +26,13 @@ library_fields = {
     'fac_other': fields.String,
     'req_speaker': fields.String,
 }
+
+library_protected_fields = {
+    'manager_name': fields.String,
+    'manager_email': fields.String,
+    'manager_phone': fields.String,
+}
+
 
 libraryReqparse = reqparse.RequestParser()
 libraryReqparse.add_argument('name', type=str, trim=True,
@@ -97,11 +101,15 @@ class LibraryListResource(Resource):
     def __init__(self):
         super().__init__()
 
-    @marshal_with(library_fields)
     def get(self):
         libraries = db.query(Library).all()
 
-        return libraries
+        is_admin = False
+        resp_fields = library_fields
+        if is_admin:
+            resp_fields = {**library_fields, **library_protected_fields}
+
+        return marshal(libraries, resp_fields)
 
     @marshal_with(library_fields)
     def post(self):
@@ -125,11 +133,15 @@ class LibraryListResource(Resource):
 
 
 class LibraryResource(Resource):
-    @marshal_with(library_fields)
     def get(self, pk):
         library = get_or_404(Library, pk)
 
-        return library
+        is_admin = False
+        resp_fields = library_fields
+        if is_admin:
+            resp_fields = {**library_fields, **library_protected_fields}
+
+        return marshal(library, resp_fields)
 
     @marshal_with(library_fields)
     def put(self, pk):
