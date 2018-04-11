d82e680036df842e6a4cf3f268f58f6ef0192338
kujyp <pjo901018@gmail.com>
2018-04-11 15:22:49 +0900

    Add create_app (#6)

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from app import app
+from app import create_app
+from app.config import Config
+
 
 if __name__ == "__main__":
+    app = create_app(Config)
     app.run(host="0.0.0.0", port=5000, debug=True)
