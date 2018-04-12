a5e8206a71688b4e9e6fb1c9d6e66f0a8c9a5250
kujyp <pjo901018@gmail.com>
2018-04-12 15:53:41 +0900

    Passed the tests

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -1,10 +1,44 @@
+import re
+
+
 def is_valid_email(field):
-    return True
+    email_pattern = "[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*"\
+        + "@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}"
+
+    if re.fullmatch(email_pattern, field):
+        return True
+
+    return False
 
 
 def has_valid_length(field, length_min, length_max):
+    if not field:
+        return False
+
+    if not (length_min <= len(field) <= length_max):
+        return False
+
     return True
 
 
 def is_valid_phone(field):
-    return True
+    landlinephone_prefix = "(02|031|032|033|041|042|043|044|" \
+                           + "051|052|053|054|055|061|062|063|064|067)"
+
+    patterns = [
+        "01[016789]{1}\d{3,4}\d{4}",
+        landlinephone_prefix + "\d{3,4}\d{4}",
+        "(0505|0504)\d{3,4}\d{4}",
+        "070\d{3,4}\d{4}",
+
+        "01[016789]{1}-?\d{3,4}-?\d{4}",
+        landlinephone_prefix + "-?\d{3,4}-?\d{4}",
+        "(0505|0504)-?\d{3,4}-?\d{4}",
+        "070-?\d{3,4}-?\d{4}",
+    ]
+
+    for pattern in patterns:
+        if re.fullmatch(pattern, field):
+            return True
+
+    return False
--- tests/unit/test_validators.py
+++ tests/unit/test_validators.py
@@ -5,23 +5,23 @@ from app.utils import validators
 
 class TestValidators(TestCase):
     def test_is_valid_email(self):
-        emails = {
+        expected = {
             "abc@gmail.com": True,
+            "1234@gmail.com": True,
             "가@gmail.com": False,
-            "1234@gmail.com": False,
             "1234@abc": False,
             "@abc.com": False,
             "@.": False,
         }
 
-        for email, expected in emails.items():
-            self.assertEqual(expected,
-                             validators.is_valid_email(email),
-                             "Email = [{}] expected [{}]"
-                             .format(email, expected))
+        actual = expected.copy()
+        for email in expected.keys():
+            actual[email] = validators.is_valid_email(email)
+
+        self.assertEqual(expected, actual)
 
     def test_is_valid_phone(self):
-        phones = {
+        expected = {
             "01012345678": True,
             "0101234567": True,
             "010123456": False,
@@ -51,15 +51,15 @@ class TestValidators(TestCase):
             "010 1234 5678": False,
         }
 
-        for phone, expected in phones.items():
-            self.assertEqual(expected,
-                             validators.is_valid_phone(phone),
-                             "Phone = [{}] expected [{}]"
-                             .format(phone, expected))
+        actual = expected.copy()
+        for phone in expected.keys():
+            actual[phone] = validators.is_valid_phone(phone)
+
+        self.assertEqual(expected, actual)
 
     def test_has_valid_length(self):
         length_range = [4, 20]
-        strings = {
+        expected = {
             "1234": True,
             "123": False,
             "12345678901234567890": True,
@@ -71,9 +71,8 @@ class TestValidators(TestCase):
             "1234567890123456789 0": False,
         }
 
-        for string, expected in strings.items():
-            self.assertEqual(
-                expected,
-                validators.has_valid_length(string, *length_range),
-                "String = [{}], boundary = [{}], expected [{}]"
-                .format(string, length_range, expected))
+        actual = expected.copy()
+        for string in expected.keys():
+            actual[string] = validators.has_valid_length(string, *length_range)
+
+        self.assertEqual(expected, actual)
