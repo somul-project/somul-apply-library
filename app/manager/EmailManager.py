d1859a1796e6f355d70dff50a8f2ec9d744236d7
harrydrippin <hj332921@gmail.com>
2018-04-14 22:04:43 +0900

    UUID Generation 부분 분리
    Signed-off-by: harrydrippin <hj332921@gmail.com>

--- app/manager/EmailManager.py
+++ app/manager/EmailManager.py
@@ -1,5 +1,4 @@
 from app.config import Config
-import uuid
 import sendgrid
 from sendgrid.helpers.mail import Email, Content, Mail
 
@@ -25,18 +24,15 @@ class EmailManager:
         return response.status_code % 100 == 2
 
     @classmethod
-    def get_uuid_content(cls):
+    def get_uuid_content(cls, uuid):
         """
         UUID를 만들고 이에 해당하는 E-mail 제목 및 본문을 만듭니다.
         """
-        generated_uuid = str(uuid.uuid4())
-
-        # TODO(@harrydrippin): UUID에 대한 중복 검사 및 재생성 로직
         subject = "[5월, 소프트웨어에 물들다] 가입 인증 메일입니다."
         content = """
         안녕하세요, 5월 소프트웨어에 물들다 운영진입니다.
 
         다음 링크를 클릭하시면 E-mail을 인증할 수 있습니다. 감사합니다.
         http://somul.kr/verify?key=%s
-        """.format(generated_uuid)
-        return subject, content, generated_uuid
+        """.format(uuid)
+        return subject, content
