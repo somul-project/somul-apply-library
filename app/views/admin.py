d76d28d592b9001d0001eed29096610c7dff89d7
kujyp <pjo901018@gmail.com>
2018-04-15 04:30:41 +0900

    Fix errors

--- app/views/admin.py
+++ app/views/admin.py
@@ -7,5 +7,5 @@ admin = Blueprint('views.admin', __name__)
 
 @admin.route("/")
 def index():
-    speakers = SpeakerInfo.query.filter_by(SpeakerInfo.admin_approved is None)
-    return render_template("admin_matching.html", speakers=speakers)
+    speakers = SpeakerInfo.query.filter_by(admin_approved=None)
+    return render_template("admin_matching.html", speakers=list(speakers))
