4ebd3cfc872badb53efca1ad420e5f40bedd43c8
kujyp <pjo901018@gmail.com>
2018-04-17 20:20:06 +0900

    Credential manager use admin_key

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv(find_dotenv())
 
 class Config:
     secret_key = os.environ.get("SECRET_KEY")
+    admin_key = os.environ.get("ADMIN_KEY")
     server_host = os.environ.get("SERVER_HOST")
     db_host = os.environ.get("DB_HOST")
     db_username = os.environ.get("DB_USERNAME")
