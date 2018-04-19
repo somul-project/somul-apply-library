38cf12273cdd988d7577d49e6bbc2b08f8a5c14c
kujyp <pjo901018@gmail.com>
2018-04-19 18:39:32 +0900

    Add admin/log page

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint
 
 from app.database.models import SpeakerInfo
+from app.v1.controllers.logger import LoggerResource
 
 admin = Blueprint('views.admin', __name__)
 
@@ -8,4 +9,10 @@ admin = Blueprint('views.admin', __name__)
 @admin.route("/")
 def index():
     speakers = SpeakerInfo.query.filter_by(admin_approved=None)
-    return render_template("admin_matching.html", speakers=list(speakers))
+    return render_template("admin/admin_matching.html", speakers=list(speakers))
+
+
+@admin.route("/log")
+def log():
+    results = LoggerResource().get()
+    return render_template("admin/admin_log.html", logs=list(results))
