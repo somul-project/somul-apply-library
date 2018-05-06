f7e6f6f46314d8e7d0320833a60b801da6b48a3e
harrydrippin <hj332921@gmail.com>
2018-05-06 17:53:07 +0900

    Add various things
    Signed-off-by: harrydrippin <hj332921@gmail.com>

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint
 
-from app.database.models import SpeakerInfo
+from app.database.models import SpeakerInfo, Library
 from app.v1.controllers.logger import LoggerResource
 
 admin = Blueprint('views.admin', __name__)
@@ -25,9 +25,63 @@ def index():
             "title": speaker.title.replace("\n", "<br>"),
             "description": speaker.description.replace("\n", "<br>")
         })
-    
+
     return render_template("admin/admin_matching.html",
                            speakers=speaker_list)
+
+
+@admin.route("/library")
+def library():
+    libraries = Library.query.all()
+    library_list = list()
+    for library in libraries:
+        library_dict = {
+            "id": library._id,
+            "name": library.name,
+            "location": {
+                "road": library.location_road,
+                "detail": library.location_detail
+            },
+            "manager": {
+                "name": library.manager_name,
+                "email": library.manager_email,
+                "phone": library.manager_phone
+            },
+            "volunteers": list(),
+            "speakers": {
+                "14:00": dict(),
+                "15:00": dict()
+            },
+        }
+
+        if library.users:
+            for user in library.users:
+                if user.speakerinfo:
+                    library_dict["speakers"][user.speakerinfo.session_time] = {
+                        "name": user.name,
+                        "email": user.email,
+                        "phone": user.phone,
+                        "title": user.speakerinfo.title,
+                        "description": user.speakerinfo.description
+                        .replace("\n", "<br>"),
+                        "introduce": user.speakerinfo.introduce
+                        .replace("\n", "<br>"),
+                        "history": user.speakerinfo.history
+                        .replace("\n", "<br>"),
+                        "keynote_link": user.speakerinfo.keynote_link,
+                        "admin_approved": user.speakerinfo.admin_approved
+                    }
+                else:
+                    library_dict["volunteers"].append({
+                        "name": user.name,
+                        "email": user.email,
+                        "phone": user.phone
+                    })
+
+        library_list.append(library_dict.copy())
+
+    return render_template("admin/admin_library_status.html",
+                           libraries=library_list)
 
 
 @admin.route("/log")
