0ccf3fd848d8612db637bfce1991abec3661ac2f
dexterastin <dexterastin@gmail.com>
2019-03-14 13:19:31 +0900

    close volunteer application page

--- app/views/volunteer.py
+++ app/views/volunteer.py
@@ -8,7 +8,7 @@ volunteer = Blueprint('views.volunteer', __name__)
 
 @volunteer.route("/")
 def application():
-    return render_template("volunteer/application.html")
+    return render_template("volunteer/application_soon.html")
 
 
 @volunteer.route("/login")
