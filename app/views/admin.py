86a800c3b8e8b058c3ee5573b3185d615fc893f2
dexterastin <dexterastin@gmail.com>
2019-04-08 12:41:36 +0900

    Add attr has_experienced_somul on Approve View Page

--- app/views/admin.py
+++ app/views/admin.py
@@ -63,7 +63,8 @@ def approve():
                 "name": speaker.user.name,
                 "library_name": speaker.user.library.name,
                 "phone": speaker.user.phone,
-                "email": speaker.user.email
+                "email": speaker.user.email,
+                "has_experienced_somul": speaker.user.has_experienced_somul
             },
             "_id": speaker._id,
             "session_time": speaker.session_time,
