8d7a2b01a2150a1d41371fd0bcec7f95cc6f40f2
dexterastin <dexterastin@gmail.com>
2019-05-13 00:14:45 +0900

    Add various things

--- app/v1/controllers/maps.py
+++ app/v1/controllers/maps.py
@@ -1,34 +1,59 @@
 from flask import Blueprint
-from flask_restful import (Resource, fields, marshal_with,
-                           Api)
+from flask_restful import (Resource, Api)
 
 from app.database.models import Library
-
-map_fields = {
-    '_id': fields.Integer,
-    'name': fields.String,
-    'location_road': fields.String,
-    'province': fields.String,
-    'location' : {
-        'latitude': fields.Float,
-        'longitude': fields.Float,
-    },
-}
 
 
 class MapListResource(Resource):
     def __init__(self):
         super().__init__()
 
-    @marshal_with(map_fields)
     def get(self):
         libraries = Library.query.all()
 
+        library_list = list()
         for library in libraries:
-            if library.location_road:
-                library.province = library.location_road[0:2]
+            library_dict = {
+                "id": library._id,
+                "name": library.name,
+                "location": {
+                    "road": library.location_road,
+                    "detail": library.location_detail,
+                    "latitude": str(library.latitude),
+                    "longitude": str(library.longitude)
+                },
+                "volunteers": list(),
+                "speakers": {
+                    "14:00": dict(),
+                    "15:00": dict()
+                }
+            }
 
-        return libraries
+            if library.users:
+                for user in library.users:
+                    if user.speakerinfo:
+                        sess_time = user.speakerinfo.session_time
+                        library_dict["speakers"][sess_time] = {
+                            "name": user.name,
+                            "email": user.email,
+                            "title": user.speakerinfo.title,
+                            "description": user.speakerinfo.description
+                            .replace("\n", "<br>"),
+                            "introduce": user.speakerinfo.introduce
+                            .replace("\n", "<br>"),
+                            "history": user.speakerinfo.history
+                            .replace("\n", "<br>")
+                            # "keynote_link": user.speakerinfo.keynote_link
+                        }
+                    else:
+                        library_dict["volunteers"].append({
+                            "name": user.name,
+                            "email": user.email
+                        })
+
+            library_list.append(library_dict.copy())
+
+        return library_list
 
 
 maps_api = Blueprint('resources.maps', __name__)
